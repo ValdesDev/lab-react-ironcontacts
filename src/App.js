8af4374036de2d934f533e780c1544cbbb1deada
JoseLValdes <jolvaldgo@gmail.com>
2021-12-01 10:38:51 +0100

    Iteration 3

--- src/App.js
+++ src/App.js
@@ -2,29 +2,45 @@ import "./App.css";
 import contacts from "./contacts.json";
 import React, { useState } from "react";
 
-
-
 function App() {
+  const remainingContacts = contacts.slice(5);
   const [contactList, setContactList] = useState(contacts.slice(0, 5));
+  function getRandomStar() {
+    const random = Math.floor(Math.random() * remainingContacts.length);
+    remainingContacts.splice(random, 1);
+    return remainingContacts[random];
+  }
+  function addStar() {
+    setContactList(contactList.concat([getRandomStar()]));
+  }
+
   return (
     <div className="App">
+      <h1>IronContacts</h1>
+      <button onClick={addStar}>Add Random Contact</button>
       <table>
-        <tr>
-          <th>Picture</th>
-          <th>Name</th>
-          <th>Popularity</th>
-          <th>Won Oscar</th>
-          <th>Won Emmy</th>
-        </tr>
-        {contactList.map((contact, i) => (
-        <tr key={i}>
-          <td><img src={contact.pictureUrl} alt={contact.name}/></td>
-          <td>{contact.name}</td>
-          <td>{contact.popularity}</td>
-          {contact.wonOscar && <td>🏆</td>}
-          {contact.wonEmmy && <td>🏆</td>}
-        </tr>
-        ))}
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won Oscar</th>
+            <th>Won Emmy</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contactList.map((contact, i) => (
+            <tr key={i}>
+              <td>
+                <img src={contact.pictureUrl} alt={contact.name} />
+              </td>
+              <td>{contact.name}</td>
+              <td>{contact.popularity}</td>
+              {contact.wonOscar && <td>🏆</td>}
+              {contact.wonEmmy && <td>🏆</td>}
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
   );
